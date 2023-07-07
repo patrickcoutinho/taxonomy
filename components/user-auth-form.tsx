e92cd3e813480a1686c06684bad72dac8e58774d
Patrick Coutinho <patrick@istyle.com.br>
2023-07-07 20:30:31 -0300

    chore: update deps

--- components/user-auth-form.tsx
+++ components/user-auth-form.tsx
@@ -15,7 +15,7 @@ import { Label } from "@/components/ui/label"
 import { toast } from "@/components/ui/use-toast"
 import { Icons } from "@/components/icons"
 
-interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> { }
 
 type FormData = z.infer<typeof userAuthSchema>
 
--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["avatars.githubusercontent.com"],
-  },
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["@prisma/client"],
-  },
+  }
 }
 
 export default withContentlayer(nextConfig)
